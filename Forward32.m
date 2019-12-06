3d7a6c23bdf551249b6b54beebccf10b0f8705a3
Vinodh N <vinodhnagarajan@outlook.com>
2019-12-06 15:46:35 -0800

    clearing branch

--- Forward32.m
+++
@@ -1,8 +0,0 @@
-function f = Forward32(brick)
-    % Forward 1 square length
-
-    brick.MoveMotorAngleRel('AB', 40, 1170, 'Coast');
-    brick.WaitForMotor('AB');
-    
-    f = true;
-end 
--- LeftTurn90.m
+++
@@ -1,9 +0,0 @@
-function L = LeftTurn90(brick)
-    % Left turn
-
-    brick.MoveMotorAngleRel('A', -20, 173, 'Brake');
-    brick.MoveMotorAngleRel('B', 20, 173 , 'Brake');
-    brick.WaitForMotor('AB');
-
-    L = true;
-end 
