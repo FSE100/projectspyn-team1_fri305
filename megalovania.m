3c074b59f4ddd4a864307a76ee6ef1c97daea713
mishebek <57020456+mishebek@users.noreply.github.com>
2019-12-06 16:49:12 -0700

    Add files via upload
    it's megalovania (the song)

---
+++ megalovania.m
@@ -0,0 +1,18 @@
+brick = legoev3;
+
+brick.playtone(20,1174,250);
+brick.playtone(20,1174,250);
+brick.playtone(20,2349,500);
+brick.playtone(20,1760,500);
+
+brick.playtone(0,1174,250);
+
+brick.playtone(20,1661,500);
+brick.playtone(20,1567,500);
+brick.playtone(20,1396,500);
+
+brick.playtone(20,1174,250);
+brick.playtone(20,1396,250);
+brick.playtone(20,1567,250);
+brick.playtone(20,1046,250);
+brick.playtone(20,1046,250);
